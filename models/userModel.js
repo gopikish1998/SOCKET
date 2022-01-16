9c595de82c08379663bf321955b6355a9b6388c2
Gopi Kishore <kishorepenjendra@gmail.com>
2022-01-16 15:39:19 +0530

    pic status updated

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique:true },
     password: { type: String, required: true },
-    pic: { type: String, required: true, default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" },
+    pic: { type: String, default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" },
         
 }, {
     timestamps: true
